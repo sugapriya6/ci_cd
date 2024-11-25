0ea5a052ebeea07f18f1e0da4f0c8698d5c598dc
sugapriya6 <161485384+sugapriya6@users.noreply.github.com>
2024-11-25 06:43:51 +0530

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        """Test the add function."""
+        self.assertEqual(add(3, 5), 8)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+if _name_ == "_main_":
+    unittest.main()
